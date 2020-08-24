6984708879fca2de73eeca383aff367aba066820
mertcankose <mertcankose97@outlook.com>
2020-08-25 01:43:57 +0300

    fix

--- app.js
+++ app.js
@@ -45,6 +45,6 @@ app.post("/",function(req,res){
   });
 });
 
-app.listen(process.env.PORT || 3000, () => {
+app.listen(process.env.PORT || 5000, () => {
   console.log(`Server is running on port ${port}.`);
 });
